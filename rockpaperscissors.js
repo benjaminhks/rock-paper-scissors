e80d8b2cb04f264116989bb6df17fba2d5ca9f72
benjaminhks <benjaminhong1996@hotmail.com>
2022-07-27 23:19:32 +0800

    Cleaned up code Cleaned up the code on " myComputerChoice" function so that it returns the array string and also trying to figure out playerSelection function, commented the rest of code out

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,23 +1,42 @@
 console.log ("Hello World")
 ;
-var rps = ["Rock",
-"Paper",
-"Scissors"
-];
 
-function myComputerChoice () {
-    return [Math.floor(Math.random()*rps.length)]; 
+var choices = ["Rock", "Paper", "Scissors"]
+
+function myComputerChoice() {
+    let result = [Math.floor(Math.random()*choices.length)]; //assigning variable result to randomise the length of the array; returning an int of the array
+    return choices[result]; //this returns the string instead of interger
+}
+
+function playerSelection () {
+    let choice = prompt ("Please enter your choice", "");//create a prompt asking for input from the user when function is run
+    if (choice === choices) { //this is to check if the input tallies with the array before executing the lines; should this be outside the function then?
+        
+    }
+
+}
+
+
+
+//function myComputerChoice () {
+//    return [Math.floor(Math.random()*rps.length)]; 
     ;    // this is currently returning just intergers...
     //referenced: https://github.com/mooniiDev/rock-paper-scissors-game/blob/master/scripts.js
     //2nd reference: https://stackoverflow.com/questions/16017657/how-do-i-generate-a-randomly-selected-word-or-phrase-from-a-previously-selecte
     //next problemt to solve is how to convert from the character string into the words itself?
-}
+//}
+//return myComputerChoice
 
-function playerSelection () {
-    return [Math.floor(Math.random()*rps.length)];
-}
-for (var x=0; x<=2;x++)
-console.log (myComputerChoice) && console.log (playerSelection) //calling both functions work; however they still return integers
+//function play() {
+//   var rps = ["Rock","Paper","Scissors"]
+//   return rps[myComputerChoice];
+//}
+
+//function playerSelection () {
+//    return [Math.floor(Math.random()*rps.length)];
+//}
+//for (var x=0; x<=2;x++)
+//console.log (myComputerChoice) && console.log (playerSelection) //calling both functions work; however they still return integers
 
 //now i have to write some conditions; I think switch statements with conditions will do well here...
 
