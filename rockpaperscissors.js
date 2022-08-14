7ca171a37f8dcedb32c7ce8168e1742572e3efa3
benjaminhks <benjaminhong1996@hotmail.com>
2022-08-15 00:41:43 +0800

    I don't know Got the playRound function working; but can't seem to get the game function working. Also, why do i have to redefine playerchoice and computechoice in game funciton, why can't i just call the established functions earlier? At least the for loop is running, but it isn't running the playRound function and thus cannot determine a winner, i'm really stumped

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,20 +1,16 @@
 console.log ("Hello World")
 ;
+var x = playerScore = 0;
+var y = computerScore = 0;
 
-var x = Player_Score = 0; 
-//var x = 0;
-var y = Computer_Score = 0;
-//var y = 0
 
 var choices = ["ROCK", "PAPER", "SCISSORS"]
 console.log (choices)
-//var playerChoice = prompt ("Please enter either Rock, Paper or Scissors")
 
-//console.log (playerChoice) //this is how to console log
 
 function myComputerChoice() {
-    let result = [Math.floor(Math.random()*choices.length)]; //assigning variable result to randomise the length of the array; returning an int of the array
-    return choices[result]; //this returns the string value instead of interger
+    let result = [Math.floor(Math.random()*choices.length)]; 
+    return choices[result]; 
 }
 
 
@@ -26,65 +22,36 @@ function playerSelection () { //the function should make the result of the promp
 
 function playRound(playerSelection, myComputerChoice){
         if (playerSelection==myComputerChoice){
-            return "Tie"
+            console.log("It's a tie");
+            return (x,y);
         }
         else if (playerSelection=="ROCK"&&myComputerChoice=="SCISSORS" || playerSelection=="PAPER"&&myComputerChoice=="ROCK" || playerSelection=="SCISSORS"&&myComputerChoice=="PAPER") {
-                console.log (x)
-                return ("Player wins the round" && x+1);} // it is just returning 0 , do i have to specify the variable to be an int before adding, or is it not as simple as a +1, there's another expression for compounding the add?
-            else {
-                console.log (y)
-                return ("Computer wins the round!" && y+1)
+            console.log ("Player wins the round");
+            return (x++);}
+        else if (playerSelection=="ROCK"&&myComputerChoice=="PAPER" || playerSelection=="PAPER"&&myComputerChoice=="SCISSORS" || playerSelection=="SCISSORS"&&myComputerChoice=="ROCK"){
+            console.log ("Computer wins the round"); 
+            return (y++);
             }
-   
-}
-function playerScore (){
-    var scoring = playRound
-    //for every time it returns "Player wins the round" count towards this variable?
-    //we need to change the string to an interger to keep score
-}
+        }
 
-function game ()
-    {for (let i= 0; i<5; i++){
-playRound(playerSelection(),myComputerChoice()) // but this just returns undefined instead of the return values above, what's with that?
-    }
-    if (x > 3){
-        return "Player has won the game!"
-    } //how to start scoring it? write a keep score function?
-    else{
-        return "Computer has won the game!"
-    }
-    //else if (playerScore=ComputerScore){
-        //replay the loop until a player has won 
-    }
 
+function game (){
+    for (let i= 0; i<5; i++) {
+    playerChoice = prompt ("Please choose Rock, Paper or Scissors");
+    playerSelection= (playerChoice.toUpperCase());
+    console.log ("Player has chosen " + playerSelection);
+    
+    choices = ["rock","paper","scissors"];
+    result = [Math.floor(Math.random()*choices.length)];
+    myComputerChoice = choices[result];
+    console.log("Computer has picked "+(myComputerChoice.toUpperCase().toUpperCase()));
+    
+    console.log(playRound(playerSelection,myComputerChoice))
+    
+    }if (playerScore>computerScore){
+            return "Player has won the game"
+    }else if (playerScore<computerScore){
+        return "Computer has won the game"
+    }
+    }
 //https://stackoverflow.com/questions/71590630/rock-paper-scissors-game-prompt-loop-and-scoreboard
-
-
-//function myComputerChoice () {
-//    return [Math.floor(Math.random()*rps.length)]; 
-    ;    // this is currently returning just intergers...
-    //referenced: https://github.com/mooniiDev/rock-paper-scissors-game/blob/master/scripts.js
-    //2nd reference: https://stackoverflow.com/questions/16017657/how-do-i-generate-a-randomly-selected-word-or-phrase-from-a-previously-selecte
-    //next problemt to solve is how to convert from the character string into the words itself?
-//}
-//return myComputerChoice
-
-//function play() {
-//   var rps = ["Rock","Paper","Scissors"]
-//   return rps[myComputerChoice];
-//}
-
-//function playerSelection () {
-//    return [Math.floor(Math.random()*rps.length)];
-//}
-//for (var x=0; x<=2;x++)
-//console.log (myComputerChoice) && console.log (playerSelection) //calling both functions work; however they still return integers
-
-//now i have to write some conditions; I think switch statements with conditions will do well here...
-
-//  if {playerselection === rock & myComputerChoice === paper; is it possible to have some for loops here? doesn't seem possible
-//      alert ("Player has won!");
-// actually should be switch statemnets here
-// else {
-
-// }
